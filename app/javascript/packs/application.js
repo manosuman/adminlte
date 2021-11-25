8b69ff672cf4eb1575226fd83739b964695d49d9
Manohar Suman <msuman@anviam.com>
2021-11-25 11:26:27 +0530

    Commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,11 +12,22 @@ import "@fortawesome/fontawesome-free/js/all";
 import '../stylesheets/application';
 import 'bootstrap';
 
+// window.$ = $;
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('admin-lte');
+require("packs/custom/jquery.vmap.min.js");
+require("packs/custom/jquery.vmap.usa.js");
+require("packs/custom/Chart.min");
+require("packs/dashboard");
+require("packs/custom/sparkline");
+require("packs/custom/daterangepicker");
+require("packs/custom/moment.min");
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
 });
-
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'login/create'
+  resources :users
   root 'home#index'
-  # get 'home/index'
+  get 'home/index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
